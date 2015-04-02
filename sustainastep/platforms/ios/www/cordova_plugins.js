98dee1844f130224fc9cc4301f66e90ca93f7238
Scott Roberts <scottroberts2@gmail.com>
2015-04-02 10:44:08 +0100

    added plugins that we need, and jquery/jquery mobile

--- sustainastep/platforms/ios/www/cordova_plugins.js
+++ sustainastep/platforms/ios/www/cordova_plugins.js
@@ -1,7 +1,50 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.core.parseplugin/www/cdv-plugin-parse.js",
+        "id": "org.apache.cordova.core.parseplugin.ParsePlugin",
+        "clobbers": [
+            "window.parsePlugin"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.apache.cordova.core.parseplugin": "0.1.0",
+    "org.apache.cordova.device": "0.3.0",
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.vibration": "0.3.13",
+    "org.apache.cordova.splashscreen": "1.0.0"
+}
 // BOTTOM OF METADATA
 });
